f726cf9989921e4ec00cfb798ad9e16d9f91644c
Chris Bagwell <49886936+CJMoneyBags@users.noreply.github.com>
2026-03-16 18:40:43 -0600

    My script test for transform.localScale

--- GuessTheNumber/MyFirstCode/Assets/MyScript.cs
+++ GuessTheNumber/MyFirstCode/Assets/MyScript.cs
@@ -5,7 +5,7 @@ public class MyScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.localScale *= 2;
     }
 
     // Update is called once per frame
